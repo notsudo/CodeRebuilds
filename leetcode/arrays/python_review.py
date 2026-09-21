8f8783833e6611687a081df4646fee858f159bab
notsudo <123038205+notsudo@users.noreply.github.com>
2026-09-21 12:55:19 -0500

    problems 1 and 2

---
+++ leetcode/arrays/python_review.py
@@ -0,0 +1,25 @@
+#Problem 1
+#problem was write an arr and print out every number greater than 4
+
+arr = [3,7,2,9,4]
+
+# forgot that range only takes a integer so u cant put in the list
+#len() => gives back the length of the arr
+
+# for i in range(len(arr)):
+#     if arr[i] > 4:
+#         print (arr[i])
+
+#in python you can directly access the number in the array and not require the index to retrieve that number
+# for number in arr:
+#     if number > 4:
+#         print( arr[i])
+
+#Problem 2
+#Add all the numbers in the arr to a sum without using the sum() function
+
+# total = 0
+# for num in arr:
+#     total = num + total
+#     #shortcut is this total += num
+# print(total)
